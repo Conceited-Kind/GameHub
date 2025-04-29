32879deb1ec06c020ec51b1571922a18751b57ac
Allan Kinuva <allankinuva@gmail.com>
2025-04-29 08:33:31 +0300

    Final commit

---
+++ GameHub/index.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+server.use(router);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`JSON Server is running on http://0.0.0.0:${port}`);
+});
